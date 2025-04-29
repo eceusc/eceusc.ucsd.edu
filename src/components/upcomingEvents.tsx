3483de6a17855f795dd6fa6b107de07e691ebad9
Jaden <jadenseangmany@gmail.com>
2025-04-28 18:52:19 -0700

    Update upcomingEvents.tsx

--- src/components/upcomingEvents.tsx
+++ src/components/upcomingEvents.tsx
@@ -10,20 +10,13 @@ export default function UpcomingEvents() {
       <div className="flex flex-col items-center text-white p-4">
         {/* Flyer Image */}
         <div className="mb-4">
-          <Image
+          {/* <Image
             src="images/industryqa.png" // place image here when needed
             alt="Flyer for ECE Day"
             width={400}
             height={300}
             className="rounded-sm"
-          />
-          <Image
-            src="images/surfing.png" // place image here when needed
-            alt="Flyer for ECE Day"
-            width={400}
-            height={300}
-            className="rounded-sm"
-          />
+          /> */}
         </div>
       </div>
     </Container>
