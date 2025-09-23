ec145f68610759f77f937864ed0e406909ef8eaf
Jaden <jadenseangmany@gmail.com>
2025-09-23 15:42:59 -0700

    updated upcoming events on website

--- src/components/upcomingEvents.tsx
+++ src/components/upcomingEvents.tsx
@@ -2,23 +2,23 @@ import Container from "./container";
 import Image from "next/image";
 
 export default function UpcomingEvents() {
-    return (
-        <Container type="1">
-            <span className="flex text-2xl md:text-4xl text-white justify-center p-8 pt-0 font-bold">
-                Upcoming Events
-            </span>
-            <div className="flex flex-col items-center text-white p-4">
-                {/* Flyer Image */}
-                <div className="mb-4">
-                    <Image
-                        src="images/fontimg.png" // place image here when needed
-                        alt="Flyer for AMA"
-                        width={800} 
-                        height={600} 
-                        className="rounded-sm"
-                    />
-                </div>
-            </div>
-        </Container>
-    );
+  return (
+    <Container type="1">
+      <span className="flex text-2xl md:text-4xl text-white justify-center p-8 pt-0 font-bold">
+        Upcoming Events
+      </span>
+      <div className="flex flex-col items-center text-white p-4">
+        {/* Flyer Image */}
+        <div className="mb-4">
+          <Image
+            src="images/fall_opener_ad.png" // place image here when needed
+            alt="Fall opener flyer"
+            width={800}
+            height={600}
+            className="rounded-sm"
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
